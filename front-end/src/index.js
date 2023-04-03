8e5236da9f3f154533c3d91219c178d33fc22445
sk LEE <znfnfn2140@gmail.com>
2023-04-03 23:25:39 +0900

    Feat 파이어베이스 로그인, 회원가입

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./router";
+import "./api/firebase"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
